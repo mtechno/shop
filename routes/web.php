8f1c442d32274c604c01b13f553d08a7da76d5f5
r.chernikov <mtechnologiya@gmail.com>
2024-06-06 00:45:05 +0300

    lesson 1

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,14 @@ use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+Route::get('/categories', function () {
+    return view('categories');
+});Route::get('/mobiles/phone_x_64', function () {
+    return view('product');
+});
+
+
 
 Route::get('/admin', AdminController::class)->name('admin');
