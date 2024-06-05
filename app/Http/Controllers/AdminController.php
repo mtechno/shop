adc17cff919fb4e69febda5468777ba9300bb3c2
r.chernikov <mtechnologiya@gmail.com>
2024-06-05 23:38:18 +0300

    init commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function __invoke()
+    {
+        return view('admin');
+        // TODO: Implement __invoke() method.
+    }
+    //
+}
